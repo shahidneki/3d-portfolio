be663c97314a9139279d66a0c71f9b2fa3a9911d
Shahid Neki <shahidneki@gmail.com>
2023-04-05 15:04:38 +0100

    chore: Added navlinks to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,8 +23,16 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer">
             Shahid Neki
+            <span className="sm:block hidden">| Software Engineer</span>
           </p>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
